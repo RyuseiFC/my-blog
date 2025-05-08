11806eac20d0d5b7a6411882e1c8fb1e56e14c88
RyuseiFC <ryu1008r@yahoo.co.jp>
2025-05-08 21:18:22 +0900

    delete await

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,13 +1,14 @@
+//
 import BlogDetail from "@/components/Blogdetail";
 import { getDetail } from "@/lib/client";
 
 export default async function BlogPostPage({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: { id: string }; // Promise型ではなく直接オブジェクト型に
 }) {
   console.log("あ", params);
-  const { id } = await params; // paramsをawaitしてからidを取り出す
+  const { id } = params; // awaitする必要なし
   const blogPost = await getDetail(id);
   return <BlogDetail blogPost={blogPost} />;
 }
