7e954187f7b6b5282f4423c69259ded06f22b52a
RyuseiFC <ryu1008r@yahoo.co.jp>
2025-05-08 12:51:28 +0900

    直したよ

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -4,8 +4,9 @@ import { getDetail } from "@/lib/client";
 export default async function BlogPostPage({
   params,
 }: {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }) {
+  console.log("あ", params);
   const { id } = await params; // paramsをawaitしてからidを取り出す
   const blogPost = await getDetail(id);
   return <BlogDetail blogPost={blogPost} />;
