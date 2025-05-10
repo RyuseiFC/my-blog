3c015a96e7c0f69face6b0a25e5713c5dd41235b
RyuseiFC <ryu1008r@yahoo.co.jp>
2025-05-10 16:04:01 +0900

    OK

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -5,9 +5,9 @@ import { getDetail } from "@/lib/client";
 export default async function BlogPostPage({
   params,
 }: {
-  params: Promise<{ id: string }>;
+  params: { id: string }; // Promise型ではなく直接オブジェクト型に
 }) {
-  const { id } = await params;
+  const { id } = params; // awaitする必要なし
   const blogPost = await getDetail(id);
   return <BlogDetail blogPost={blogPost} />;
 }
